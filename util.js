445761901b48aafa79c071fae448aed7d896c64c
IceSword8492 <cleared.for.approach.runway23@gmail.com>
2019-07-15 15:51:15 +0900

    init

--- util.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    createMessage: (status, message) => `[rconclient/${status}]: ${message}`,
-    info: message => console.log(module.exports.createMessage("INFO", message)),
-    warn: message => console.warn(module.exports.createMessage("WARN", message)),
-    error: message => console.error(module.exports.createMessage("ERROR", message))
-}
